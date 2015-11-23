2650f0735187753864652cc20bd0444ac8d7c293
jaydenuk <jayden.uk@daumkakao.com>
2015-11-23 11:01:58 +0900

    init project

---
+++ src/main/java/pub/jayden/boot/main/controller/MainController.java
@@ -0,0 +1,16 @@
+package pub.jayden.boot.main.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jaydenuk on 2015. 11. 15..
+ */
+@RestController("/")
+public class MainController {
+
+    @RequestMapping("")
+    public String index() {
+        return "Hello!! Spring Boot!!";
+    }
+}
